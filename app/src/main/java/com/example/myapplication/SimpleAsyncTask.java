35ac276b7c715b154b6d9bccd2e6dd38053abf05
Isaac Bevis <43886125+IsaacBevis123@users.noreply.github.com>
2020-03-12 10:26:13 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/SimpleAsyncTask.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.Random;
+
+public class SimpleAsyncTask extends AsyncTask<Void, Integer, String> {
+    private WeakReference<TextView> mTextView;
+
+    SimpleAsyncTask(TextView tv) {
+        mTextView = new WeakReference<>(tv);
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        Random r = new Random();
+        int n = r.nextInt(11);
+
+        int s = n * 200;
+
+        publishProgress(s);
+
+        try {
+            Thread.sleep(s);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "Awake at last after sleeping for " + s + " milliseconds!";
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... progress){
+        mTextView.get().setText(mTextView.get().getText() + " for " + progress[0] + " milliseconds");
+    }
+
+    protected void onPostExecute(String result) {
+        mTextView.get().setText(result);
+    }
+}
